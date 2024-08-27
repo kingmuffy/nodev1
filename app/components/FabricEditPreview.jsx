06ed46fd9d8fcad0215158211e113ff7f0cba247
kingmuffy <kingmuffyatse@gmail.com>
2024-08-27 13:32:08 +0100

    edit preview added

--- app/components/FabricEditPreview.jsx
+++ app/components/FabricEditPreview.jsx
@@ -107,11 +107,8 @@ const FabricEditPreview = ({ parameters }) => {
 
             Object.entries(connectedMaps).forEach(([mapType, fileOrUrl]) => {
               if (fileOrUrl) {
-                console.log(`Loading texture for ${mapType}:`, fileOrUrl); // Log texture loading
                 const textureLoader = loader.load(
-                  typeof fileOrUrl === "string"
-                    ? fileOrUrl
-                    : URL.createObjectURL(fileOrUrl),
+                  `${fileOrUrl}?r=${Math.floor(Math.random() * 100000)}`, // Cache busting
                   (texture) => {
                     texture.colorSpace = SRGBColorSpace;
                     texture.needsUpdate = true;
